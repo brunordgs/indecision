d303f4a5312166674b275e18b4c95bd543651b02
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-08-05 10:11:05 -0300

    Fix env variables

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,8 +8,8 @@ export default (req: NextApiRequest, res: NextApiResponse) =>
 	NextAuth(req, res, {
 		providers: [
 			GitHubProvider({
-				clientId: process.env.GITHUB_ID as string,
-				clientSecret: process.env.GITHUB_SECRET as string,
+				clientId: process.env.NEXT_PUBLIC_GITHUB_ID as string,
+				clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET as string,
 			}),
 		],
 		debug: !isProd,
